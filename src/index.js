1d43f523337ace379740e9778312f398c3f9f1d4
ValdRomanuV <vladromanuk12vv@gmail.com>
2019-02-20 01:06:40 +0300

    gradus commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 module.exports = function warmup(temperature) {
 // your implementation here 
-	var celcGradus = +prompt('Enter your number to convert from Celsius to Fahrenheit', 0); 
 	var umn = 9/5; 
 	var sum = 32; 
 
-	return (celcGradus * umn) + sum; 
+	return (temperature * umn) + sum; 
 
 }; 
 
