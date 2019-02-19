aa465ac2160622672e32b1263ef53a24b11613fc
ValdRomanuV <vladromanuk12vv@gmail.com>
2019-02-20 00:24:58 +0300

    m

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-// module.exports = 
-function warmup(temperature) {
+module.exports = function warmup(temperature) {
 // your implementation here 
 	var celcGradus = +prompt('Enter your number to convert from Celsius to Fahrenheit', 0); 
 	var umn = 9/5; 
